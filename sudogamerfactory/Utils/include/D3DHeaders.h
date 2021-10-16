ceb9dc015da9e3e04a55e0ea663a91caf195ba00
sudogamer <nagnath.chippa@gmail.com>
2021-10-16 20:20:48 +0530

    Remove duplicate header includes from D3DHeaders.h

--- sudogamerfactory/Utils/include/D3DHeaders.h
+++ sudogamerfactory/Utils/include/D3DHeaders.h
@@ -1,19 +1,4 @@
 #pragma once
-#include <wrl.h>
-#include <dxgi1_4.h>
-#include <d3d12.h>
-#include <D3Dcompiler.h>
-#include <DirectXMath.h>
-#include <DirectXColors.h>
-#include<DirectXMath.h>
-#include<DirectXPackedVector.h>
-#include <DirectXPackedVector.h>
-#include <DirectXColors.h>
-#include<dxgi.h>
-#include<stdio.h>
-#include<iostream>
-#include<vector>
-#include <sstream>
 
 #include"factoryUtils/d3dUtil.h"
 #include"factoryUtils/d3dx12.h"
